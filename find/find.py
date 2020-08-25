db315748539aaa88d59f56e0e4e7ca528f6ded87
Jackie <jlauravelez@gmail.com>
2020-08-25 18:39:41 +0200

    pushing latest save

--- find/find.py
+++ find/find.py
@@ -13,19 +13,28 @@ parser.add_argument('strings', metavar='F', type=str, nargs='+',
         help="a file for the accumulator")
 
 parser.add_argument('--P', action='store', type=str, required=False)
+
+
 args = parser.parse_args()
 
 files = []
 paths = []
+path = False
 for i in range(len(args.strings)):
     if(len(args.strings[i].split('.')) > 1):
         files.append(args.strings[i])
-    if(len(args.strings[i].split('/')) > 1):
-        if(os.path.lexists(args.strings[i])):
-            paths.append(args.strings[i])
+
+for p in range(len(args.P)):
+    if(len(args.P[p].split('/')) > 1):
+        if(os.path.lexists(args.P[p])):
+           path = True 
 
 for j in range(len(files)):
     # prints if the file is there
     glob.glob(files[j])
 
-print(args.P)
+if(path == True):
+    test = subprocess.Popen("ls",cwd=args.P)
+    pop = test.communicate()
+
+    print("ADSFASDFASD",pop,pop)
